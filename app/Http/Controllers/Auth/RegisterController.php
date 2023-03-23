c310bdcbb950987a96c6258bf66679e6e7073bcf
Jerry Seyi <jerryseyi68@gmail.com>
2023-03-24 00:39:26 +0100

    authentication register

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed'],
+        ]);
+
+        $user = User::create($credentials);
+
+        $token = $user->createToken('auth_token');
+
+        return response()->json([
+           'user' => $user,
+           'token' => $token
+        ]);
+    }
+}
--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,6 +8,6 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        
+
     }
 }
